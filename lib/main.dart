1ddce54bd0b096db6b40df3bbc5cc5cea59f3662
fionicholas <fiorentcheaters@gmail.com>
2020-08-20 16:48:59 +0700

    implement movie up coming

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:movie_bloc_retrofit/api/api_repository.dart';
 import 'package:movie_bloc_retrofit/bloc/movie_bloc_observer.dart';
 import 'package:movie_bloc_retrofit/bloc/popular/movies_popular_bloc.dart';
+import 'package:movie_bloc_retrofit/bloc/upcoming/movies_upcoming_bloc.dart';
 import 'package:movie_bloc_retrofit/ui/home/movie_main_pages.dart';
-import 'package:movie_bloc_retrofit/ui/movie_popular_pages.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -22,10 +22,19 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home:
-      BlocProvider(
-        create: (context) {
-          return MoviePopularBloc(repository: ApiRepository());
-        },
+      MultiBlocProvider(
+        providers: [
+          BlocProvider<MoviePopularBloc>(
+            create: (context) {
+              return MoviePopularBloc(repository: ApiRepository());
+            },
+          ),
+          BlocProvider<MovieUpComingBloc>(
+            create: (context) {
+              return MovieUpComingBloc(repository: ApiRepository());
+            },
+          ),
+        ],
         child: MovieMainPages(),
       ),
     );
--- lib/ui/home/movie_main_pages.dart
+++ lib/ui/home/movie_main_pages.dart
@@ -4,6 +4,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movie_bloc_retrofit/bloc/popular/movies_popular_bloc.dart';
 import 'package:movie_bloc_retrofit/bloc/popular/movies_popular_event.dart';
 import 'package:movie_bloc_retrofit/bloc/popular/movies_popular_state.dart';
+import 'package:movie_bloc_retrofit/bloc/upcoming/movies_upcoming_bloc.dart';
+import 'package:movie_bloc_retrofit/bloc/upcoming/movies_upcoming_event.dart';
+import 'package:movie_bloc_retrofit/bloc/upcoming/movies_upcoming_state.dart';
 import 'package:movie_bloc_retrofit/ui/shimmer_movies.dart';
 import 'package:movie_bloc_retrofit/model/movies_item.dart';
 import 'package:movie_bloc_retrofit/ui/utils/card_movie_home.dart';
@@ -20,6 +23,7 @@ class _MovieMainPagesState extends State<MovieMainPages> {
   void initState() {
     super.initState();
     BlocProvider.of<MoviePopularBloc>(context).add(LoadPopularMovie());
+    BlocProvider.of<MovieUpComingBloc>(context).add(LoadUpComingMovie());
   }
 
   @override
@@ -38,7 +42,7 @@ class _MovieMainPagesState extends State<MovieMainPages> {
             SizedBox(
               height: 16.0,
             ),
-            _itemPopular(context),
+            _itemUpComing(context),
             SizedBox(
               height: 16.0,
             )
@@ -106,6 +110,83 @@ class _MovieMainPagesState extends State<MovieMainPages> {
                   ),
                 );
               } else if (state is MoviesPopularNoInternetConnection) {
+                return Container(
+                  child: Center(
+                    child: Text(state.message),
+                  ),
+                );
+              } else {
+                return Container(
+                  child: Center(
+                    child: Text("Error"),
+                  ),
+                );
+              }
+            },
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _itemUpComing(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.only(left: 4.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                'Up Coming Movie',
+                style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold),
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.arrow_forward,
+                  size: 16.0,
+                ),
+                onPressed: () {},
+              )
+            ],
+          ),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.width / 1.8,
+          child: BlocBuilder<MovieUpComingBloc, MoviesUpComingState>(
+            builder: (context, state) {
+              if (state is MoviesUpComingHasData) {
+                return ListView.builder(
+                    shrinkWrap: true,
+                    physics: ClampingScrollPhysics(),
+                    scrollDirection: Axis.horizontal,
+                    itemCount: state.movieList.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      MoviesItem movie = state.movieList[index];
+                      return CardMovieHome(
+                          image: movie.posterPath,
+                          title: movie.title,
+                          voteAverage: movie.voteAverage,
+                          onTap: (){
+                          }
+                      );
+                    });
+              } else if (state is MoviesUpComingLoading) {
+                return ShimmerMovies();
+              } else if (state is MoviesUpComingError) {
+                return Container(
+                  child: Center(
+                    child: Text(state.errorMessage),
+                  ),
+                );
+              } else if (state is MoviesUpComingNoData) {
+                return Container(
+                  child: Center(
+                    child: Text(state.message),
+                  ),
+                );
+              } else if (state is MoviesUpComingNoInternetConnection) {
                 return Container(
                   child: Center(
                     child: Text(state.message),
