1a382b3c1709347eab134defd808b554460d51f9
fionicholas <fiorentcheaters@gmail.com>
2020-09-23 00:16:20 +0700

    update UI card list movies favorite

--- lib/ui/utils/components/card_list_favorite.dart
+++ lib/ui/utils/components/card_list_favorite.dart
@@ -1,29 +1,39 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_bloc_retrofit/bloc/listmoviesfavorite/listmovies_favorite_bloc.dart';
+import 'package:movie_bloc_retrofit/bloc/listmoviesfavorite/listmovies_favorite_event.dart';
 import 'package:movie_bloc_retrofit/data/moviesfavorite/local/response/movies_favorite_entity.dart';
 import 'package:movie_bloc_retrofit/ui/detailnew/detail_new_screen.dart';
 import 'package:movie_bloc_retrofit/ui/utils/components/ErrorImage.dart';
 import 'package:movie_bloc_retrofit/ui/utils/components/LoadingIndicator.dart';
 import 'package:movie_bloc_retrofit/ui/utils/ext/common_ext.dart';
 
-class CardListFavorite extends StatelessWidget {
+class CardListFavorite extends StatefulWidget {
   const CardListFavorite({Key key, this.listMovieFavorite}) : super(key: key);
 
   final List<MoviesFavoriteEntity> listMovieFavorite;
 
   @override
+  _CardListFavoriteState createState() => _CardListFavoriteState();
+}
+
+class _CardListFavoriteState extends State<CardListFavorite> {
+  @override
   Widget build(BuildContext context) {
     return Container(
       child: ListView.builder(
-          itemCount: listMovieFavorite.length,
+          itemCount: widget.listMovieFavorite.length,
           itemBuilder: (context, index) {
-            MoviesFavoriteEntity movies = listMovieFavorite[index];
+            MoviesFavoriteEntity movies = widget.listMovieFavorite[index];
             return Padding(
               padding: const EdgeInsets.all(8.0),
               child: InkWell(
                 onTap: () {
                   Navigator.pushNamed(context, DetailNewScreen.routeName,
-                      arguments: movies.toMovie());
+                      arguments: movies.toMovie()).then((value) => setState(() {
+                    context.bloc<ListMoviesFavoriteBloc>().add(GetListMoviesFavorite());
+                  }));
                 },
                 child: Card(
                   elevation: 4.0,
